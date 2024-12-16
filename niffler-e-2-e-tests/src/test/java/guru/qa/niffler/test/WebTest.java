dfa9b4ecbf9d31765a155ae0f899ebefb40cbfa9
i.kazantcev <i.kazantcev@bftcom.com>
2024-12-16 18:35:05 +0300

    Добавил тесты по редактированию и удалению траты

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/WebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/WebTest.java
@@ -71,5 +71,6 @@ public class WebTest {
                 .acceptDelButtonClick()
                 .searchSpend(spendingJson.description())
                 .resultTableShouldBeNotVisibleCheck();
+
     }
 }
