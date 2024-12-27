0429269d7feba3b501ed77860c2f980d2f0ad15b
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-12-27 22:05:18 +0300

    Добавлена аннотация запуска/игнора теста на основании открытого/закрытого issue на Гитхаб

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/tests/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/tests/web/SpendingWebTest.java
@@ -1,5 +1,6 @@
 package guru.qa.niffler.tests.web;
 
+import guru.qa.niffler.jupiter.annotation.DisabledByIssue;
 import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.model.SpendJson;
@@ -18,6 +19,7 @@ public class SpendingWebTest {
             description = "Обучение Advanced 2.0",
             amount = 79990
     )
+    @DisabledByIssue("5")
     @Test
     void categoryDescriptionShouldBeChangedFromTable(SpendJson spend) {
         final String newDescription = "Обучение Niffler Next Generation";
