b0a294911428983b38b9b8e2959eddaf3751c872
Artur <116442638+ArturAkopov@users.noreply.github.com>
2025-01-03 22:53:54 +0300

    Реализованы новые методы для работы с БД и реализованы в DAO

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/CategoryDaoJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/CategoryDaoJdbc.java
@@ -6,6 +6,8 @@ import guru.qa.niffler.data.dao.CategoryDao;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -61,13 +63,81 @@ public class CategoryDaoJdbc implements CategoryDao {
                         ce.setUsername(rs.getString("username"));
                         ce.setName(rs.getString("name"));
                         ce.setArchived(rs.getBoolean("archived"));
-                        return Optional.of(
-                                ce
-                        );
+                        return Optional.of(ce);
                     } else {
                         return Optional.empty();
                     }
                 }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Optional<CategoryEntity> findCategoryByUsernameAndCategoryName(String username, String categoryName) {
+        try (Connection connection = Databases.connection(config.spendJdbcUrl())) {
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "SELECT * FROM category WHERE username = ? AND name = ?"
+            )) {
+                ps.setString(1, username);
+                ps.setString(2, categoryName);
+                ps.execute();
+
+                try (ResultSet rs = ps.getResultSet()) {
+                    CategoryEntity ce = new CategoryEntity();
+                    if (rs.next()) {
+                        ce.setId(rs.getObject("id", UUID.class));
+                        ce.setName(rs.getString("name"));
+                        ce.setUsername(rs.getString("username"));
+                        ce.setArchived(rs.getBoolean("archived"));
+                        return Optional.of(ce);
+                    } else {
+                        return Optional.empty();
+                    }
+                }
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<CategoryEntity> findAllByUsername(String username) {
+        try (Connection connection = Databases.connection(config.spendJdbcUrl())) {
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "SELECT * FROM category WHERE username = ?"
+            )) {
+                ps.setString(1, username);
+                ps.execute();
+
+                try (ResultSet rs = ps.getResultSet()) {
+                    List<CategoryEntity> categoryEntityList = new ArrayList<>();
+                    while (rs.next()) {
+                        CategoryEntity ce = new CategoryEntity();
+                        ce.setId(rs.getObject("id", UUID.class));
+                        ce.setName(rs.getString("name"));
+                        ce.setUsername(rs.getString("username"));
+                        ce.setArchived(rs.getBoolean("archived"));
+                        categoryEntityList.add(ce);
+                    }
+                    return categoryEntityList;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteCategory(CategoryEntity category) {
+        try (Connection connection = Databases.connection(config.spendJdbcUrl())) {
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "DELETE FROM category WHERE id = ?"
+            )) {
+                ps.setObject(1, category.getId());
+                ps.execute();
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
