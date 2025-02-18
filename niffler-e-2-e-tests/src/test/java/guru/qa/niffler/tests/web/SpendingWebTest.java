54886c93d849e5dd2ec7a4d8240cc7895383fa2e
Artur <116442638+ArturAkopov@users.noreply.github.com>
2025-02-18 23:44:09 +0300

    Allure Lifecycle, Screenshot tests

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/tests/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/tests/web/SpendingWebTest.java
@@ -1,16 +1,25 @@
 package guru.qa.niffler.tests.web;
 
 import guru.qa.niffler.jupiter.annotation.DisabledByIssue;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.utils.RandomDataUtils;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 public class SpendingWebTest {
@@ -61,6 +70,26 @@ public class SpendingWebTest {
                 .checkAlertMessage("New spending is successfully created")
                 .getSpendingTable()
                 .checkTableContains(description);
+    }
+
+    @User(
+            spendings = @Spending(
+                    category = "Обучение",
+                    description = "Обучение Advanced 2.0",
+                    amount = 79990
+            )
+    )
+    @DisplayName("Проверка компонента статистики")
+    @ScreenShotTest("img/expected-stat.png")
+    void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+        new LoginPage()
+                .open()
+                .login(user.username(), user.testData().password());
+        BufferedImage actual = ImageIO.read(Objects.requireNonNull($("canvas[role='img']").screenshot()));
+        assertFalse(new ScreenDiffResult(
+                expected,
+                actual
+        ));
     }
 }
 
